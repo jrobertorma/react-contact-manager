f6adda453a25fba3029364764496779a762a3564
J. Roberto Ramirez <jrobertorma@gmail.com>
2021-01-24 09:49:11 -0600

    New contact form added

--- src/components/contact-card.js
+++ src/components/contact-card.js
@@ -22,24 +22,6 @@ export default function ContactCard( {contact, deleteContact} ) {
     const classes = useStyles();
 
     return (
-        // <Card>
-        //     <CardContent>
-        //         <CardHeader>
-        //             <Icon name='user outline'/> {contact.name.first} {contact.name.last}
-        //         </CardHeader>
-        //         <CardDescription>
-        //             <p><Icon name='phone'/> {contact.phone}</p>
-        //             <p><Icon name='mail outline'/> {contact.email}</p>
-        //         </CardDescription>
-        //     </CardContent>
-        //     <CardContent extra>
-        //         <div className="ui two buttons">
-        //             <Button basic color="green">Edit</Button>
-        //             <Button basic color="red">Delete</Button>
-        //         </div>
-        //     </CardContent>
-        // </Card>
-
         <Card className={classes.root}>
             <CardContent>
                 <Typography variant="body2" component="p">
--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -1,126 +1,156 @@
 import React, { Component } from 'react';
-import { Form, Field } from 'react-final-form';
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
+import { Form } from 'react-final-form';
 
-const required = value => (value ? undefined : 'Required');
+//import TextField from '@material-ui/core/TextField';
+//import Grid from '@material-ui/core/Grid';
 
-const TextFieldAdapter = ({ input, meta, ...rest }) => (
-    <TextField
-        {...input}
-        {...rest}
-        onChange={(event, value) => input.onChange(value)}
-        errorText={meta.touched ? meta.error : ''}
-    />
-)
+import {
+    Paper,
+    Grid,
+    Button
+} from '@material-ui/core';
+
+import {
+    TextField,
+    makeValidate
+} from 'mui-rff';
+
+import * as Yup from 'yup';
+
+import { client } from '../actions/'; //llamas a actions/index.js, la conexión al servidor local
+
+const url = '/contacts';
 
 class ContactForm extends Component {
 
   render() {
-    const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+    //const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
     const onSubmit = async values => {
-      await sleep(300)
-      window.alert(JSON.stringify(values, 0, 2))
+        const fName = values.firstName;
+        const lName = values.lastName;
+        const uPhone = values.phone;
+        const uMail = values.email;
+
+        const body = {
+            "name": {
+                "first": fName,
+                "last": lName
+            },
+            "phone": uPhone,
+            "email": uMail
+        };
+
+        // console.log(body);
+        client.post(url, body)
+        .then(
+            window.alert("Contact added")
+        )
+        .catch(err => console.log(err.response));
     }
 
+    const phoneRegExp = /^\+{1}(\d{0,4})?\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{4}\)?)?$/;
+
+    // We define our schema based on the same keys as our form:
+    const schema = Yup.object().shape({
+        firstName: Yup.string().required('Required field'),
+        phone: Yup.string().required('Required field').matches(phoneRegExp, 'Phone number is not valid please use this notation: +5555555555'),
+        email: Yup.string().required('Required field').email('Must be a valid email'),
+    });
+
+    // Run the makeValidate function...
+    const validate = makeValidate(schema);
+
+    const formFields = [
+        {
+            size: 6,
+            field: (
+                <TextField
+                    label="First Name"
+                    name="firstName"
+                    margin="none"
+                    required={true}
+                />
+            ),
+        },
+        {
+            size: 6,
+            field: (
+                <TextField
+                    label="Last Name"
+                    name="lastName"
+                    margin="none"
+                />
+            ),
+        },
+        {
+            size: 6,
+            field: (
+                <TextField
+                    label="Phone"
+                    name="phone"
+                    margin="none"
+                    required={true}
+                />
+            ),
+        },
+        {
+            size: 12,
+            field: (
+                <TextField
+                    type="email"
+                    label="Email"
+                    name="email"
+                    margin="none"
+                    required={true}
+                />
+            ),
+        },
+    ];
+
     return (
-        <Grid 
-            container
-            direction="row"
-            justify="center"
-            alignItems="center"
-        >
+        <div>
             <h1 style={{marginTop:"1em"}}>Add New Contact</h1>
             <Form
                 onSubmit={onSubmit}
-
-                validate={values => {
-                    const errors = {}
-                    if (!values.username) {
-                        errors.username = 'Required'
-                    }
-                    if (!values.password) {
-                        errors.password = 'Required'
-                    }
-                    if (!values.confirm) {
-                        errors.confirm = 'Required'
-                    } else if (values.confirm !== values.password) {
-                        errors.confirm = 'Must match'
-                    }
-                    return errors
-                }}
-
-                render={
-                    //nota que es una arrow function (args) => {//function content}
-                    ({ handleSubmit, form, submitting, pristine, values }) => (
-                    
-                    <form onSubmit={handleSubmit}>
-
-                        <Field
-                            name="username"
-                            component={TextFieldAdapter}
-                            validate={required}
-                            hintText="First Name"
-                            floatingLabelText="First Name"    
-                        />
-
-                        <Field name="lastname">
-                            {
-                            //nota que es una arrow function (args) => {//function content}
-                            ({ input, meta }) => (
-                            <div className="ui input">
-                                <label>Last Name</label>
-                                <input {...input} type="text" placeholder="Last Name"/>
-                                {/* 'condition && <p>markup</p>', al parecer la condición trae un 'and' en el primer && 
-                                    en pocas palabras, si encuentra valores en las dos variables meta, pinta el error en un <span>*/}
-                                {meta.error && meta.touched && <span className = "error">{meta.error}</span>}
-                            </div>
-                            )}
-                        </Field>
-
-                        <Field name="password">
-                            {({ input, meta }) => (
-                            <div>
-                                <label>Password</label>
-                                <input {...input} type="password" placeholder="Password" />
-                                {meta.error && meta.touched && <span className = "error">{meta.error}</span>}
-                            </div>
-                            )}
-                        </Field>
-
-                        <Field name="confirm">
-                            {({ input, meta }) => (
-                            <div>
-                                <label>Confirm</label>
-                                <input {...input} type="password" placeholder="Confirm" />
-                                {meta.error && meta.touched && <span className = "error">{meta.error}</span>}
-                            </div>
-                            )}
-                        </Field>
-
-                        <div className="buttons">
-                            <Button primary type="submit" disabled={submitting}>
-                                Submit
-                            </Button>
-                            <button
-                                type="button"
-                                onClick={form.reset}
-                                disabled={submitting || pristine}
-                            >
-                                Reset
-                            </button>
-                        </div>
-
-                        <pre>{JSON.stringify(values, 0, 2)}</pre>
-                    
-                    </form>
-                    ) //arrow function
-                }//render
-            /> {/*form*/}
-        </Grid>
-    )
+                validate={validate}
+                render={({ handleSubmit, form, submitting, pristine, values }) => (
+                <form onSubmit={ event => { handleSubmit().then(form.reset) } } noValidate>
+                    <Paper style={{ padding: 16 }}>
+                        <Grid container alignItems="flex-start" spacing={2}>
+                            {formFields.map((item, idx) => (
+                            <Grid item xs={item.size} key={idx}>
+                                {item.field}
+                            </Grid>
+                            ))}
+                            <Grid item style={{ marginTop: 16 }}>
+                                <Button
+                                    type="button"
+                                    variant="contained"
+                                    onClick={form.reset}
+                                    disabled={submitting || pristine}
+                                >
+                                    Reset
+                                </Button>
+                            </Grid>
+                            <Grid item style={{ marginTop: 16 }}>
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    type="submit"
+                                    disabled={submitting}
+                                >
+                                    Submit
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                    <pre>{JSON.stringify(values, 0, 2)}</pre>
+                </form>
+                )}
+            /> {/*Form*/}
+        </div>
+    ) /*return */
   }
 }
 
@@ -131,4 +161,5 @@ export default ContactForm;
  * a contact-actions.js y que se active el dispatch para nuevo contacto.
  * 
  * El ejemplo del formulario funciona, falta darle estilos chidos y cablearlo con redux
+ * 
  */
